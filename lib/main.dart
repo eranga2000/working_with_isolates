0b556495e6b8a2e8140bdf3570431cb2e5d112f1
eranga2000 <dmemadhushan@std.appsc.sab.ac.lk>
2025-04-17 09:08:25 +0530

    performing a high complex task to see how it freeze the UI

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,40 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image.asset('assets/loading.gif'),
+                //UI blocking task
+                ElevatedButton(onPressed: () {
+                  // Simulate a complex task
+                 var total= complexTask1();
+                 debugPrint("Total: $total");
+                }, child: Text("Task 1")),
+
+//Let's use isolates
+
+                ElevatedButton(onPressed: () {}, child: Text("Task 2")),
+                ElevatedButton(onPressed: () {}, child: Text("Task 3")),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  double complexTask1() {
+    var total = 0.0;
+    for (int i = 0; i < 1000000; i++) {
+      total += i;
+    }
+    return total;
   }
 }
